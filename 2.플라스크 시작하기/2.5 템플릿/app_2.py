94a8edab633befde5a64fabcd368fa590a69ab18
jsmount <park20312@naver.com>
2025-02-03 09:24:14 +0900

    업로드드

---
+++ 2.플라스크 시작하기/2.5 템플릿/app_2.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/fruits')
+def show_fruits():
+    # 여기에 테스트할 과일 목록을 넣습니다.
+    fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry']
+    return render_template('fruits_list.html', fruits=fruits)
+
+if __name__ == '__main__':
+    app.run(debug=False)
